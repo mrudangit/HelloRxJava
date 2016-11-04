97b16e7c9ec3bb2b9c5e372f46a9403ebd80a2b5
majormud <mruang@yahoo.com>
2016-11-03 23:41:12 -0400

    code cleanup

--- src/main/java/com/solutionarchitects/Conflation.java
+++ src/main/java/com/solutionarchitects/Conflation.java
@@ -12,87 +12,56 @@ import java.util.concurrent.TimeUnit;
 
 class Conflation {
 
-
     private static Logger logger = LoggerFactory.getLogger(ConflationSubscriber.class.getName());
 
-
-    static <T> Observable<T> create(Observable<T> source, long timeout, Scheduler scheduler)
-    {
+    static <T> Observable<T> create(Observable<T> source, long timeout, Scheduler scheduler) {
 
         logger.info("Creating Conflation TimeOut : {}", timeout);
 
         return Observable.create(subscriber -> {
 
-
             final long[] lastUpdateTime = {0};
-            Object lock = new Object();
-
+            final Object lock = new Object();
             final Subscription[] scheduled = new Subscription[1];
 
             source.observeOn(scheduler).subscribe(t -> {
 
-
                 long currentUpdateTime = System.currentTimeMillis();
-
                 boolean scheduleRequired;
 
-                synchronized (lock){
+                synchronized (lock) {
 
-                    scheduleRequired = currentUpdateTime- lastUpdateTime[0] < timeout;
+                    scheduleRequired = currentUpdateTime - lastUpdateTime[0] < timeout;
 
-
-                    if( scheduleRequired && scheduled[0] != null){
+                    if (scheduleRequired && scheduled[0] != null) {
                         scheduled[0].unsubscribe();
-                        scheduled[0]=null;
+                        scheduled[0] = null;
                     }
-
 
                 }
 
-
-                if(scheduleRequired){
+                if (scheduleRequired) {
 
                     scheduled[0] = scheduler.createWorker().schedule(() -> {
-
 
                         subscriber.onNext(t);
 
                         synchronized (lock) {
-
                             lastUpdateTime[0] = System.currentTimeMillis();
-                            scheduled[0]=null;
-
-
+                            scheduled[0] = null;
                         }
 
                     }, timeout, TimeUnit.MILLISECONDS);
 
-
-
                 } else {
 
                     subscriber.onNext(t);
-                    synchronized (lock){
-                        lastUpdateTime[0]=System.currentTimeMillis();
+                    synchronized (lock) {
+                        lastUpdateTime[0] = System.currentTimeMillis();
                     }
-
                 }
 
-
-
-
-
-
             });
-
-
-
-
         });
-
-
-
-
-
     }
 }
